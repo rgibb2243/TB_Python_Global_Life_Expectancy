d49c050aeae7e1bcfd87046a1119f6ed796fbd73
Ryan Gibbons <ryan.gibbons1@outlook.com>
2020-08-26 23:40:54 +0100

    Added initial import script for the data.

---
+++ Initail_Data_Import.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+OutputDF = pd.read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2018/2018-07-03/week14_global_life_expectancy.csv")
+
+print(OutputDF)
